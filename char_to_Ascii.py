961cbec6dce5ec72baf13481b26b0c2f3271d501
yegane-AI <51883127+yegane-AI@users.noreply.github.com>
2019-08-14 22:32:27 +0430

    Create char_to_Ascii.py

---
+++ char_to_Ascii.py
@@ -0,0 +1,4 @@
+capital_list=["A","B","C","D","E","F","G","H",'I','J','K','L','M','N','O','P','Q','R',"S","T","U","V","W","X","Y","Z"]
+for i in capital_list:
+    l = i.lower()
+    print("The Ascii code of ",i," is ",ord(i) , " and the Ascii code of ",l," is ", ord(l) )
